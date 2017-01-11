10a57067b0a82c44dde1893f8ff9487f59d9f32d
Phelan Lewis <phelanlewis@gmail.com>
2017-01-11 19:46:54 +0000

    Created function and it has empty object

---
+++ character-counting.js
@@ -0,0 +1,13 @@
+function countLetters(str){
+  var comparingLetterResult = {};
+  var key = '';
+    for(var i = 0; i < str.length; i++){
+      if(i !== key){
+        return (i ": " i)
+      } else {
+        return (i ": " i)
+      }
+    }
+}
+
+countLetters("lighthouse in the house")
