89eccfa2a65d28ee9e6ab6781065a019d5ab70ae
Phelan Lewis <phelanlewis@gmail.com>
2017-01-11 21:17:31 +0000

    Function works but does not add compared letter to it

--- character-counting.js
+++ character-counting.js
@@ -1,13 +1,13 @@
 function countLetters(str){
-  newStr = str.split("").join("");
+  newStr = str.split(' ').join('');
   for(var i = 0; i < newStr.length; i++){
     var letter = newStr[i];
-    if (output[i] === newStr[i]) {
+    if(output.hasOwnProperty(letter)){
       output[letter] += 1;
-    } else {
+    }  else {
         output[letter] = 1;
+      }
     }
-  }
   console.log(output);
 };
 var output = {
