1578e627d3e30d56b883db941845907a47e2a0ab
Phelan Lewis <phelanlewis@gmail.com>
2017-01-11 20:35:38 +0000

    Changed for loop and added object logic

--- character-counting.js
+++ character-counting.js
@@ -1,13 +1,17 @@
 function countLetters(str){
-  var comparingLetterResult = {};
-  var key = '';
-    for(var i = 0; i < str.length; i++){
-      if(i !== key){
-        return (i ": " i)
-      } else {
-        return (i ": " i)
-      }
+  newStr = str.split("");
+  for(var i = 0; i < newStr.length; i++){
+    if (newStr[i] === output[i]) {      // wat is dis?  fix dis
+      var letter = newStr[i];
+      output[letter] += 1;
+    } else {
+        output[letter] = 1;
+      // return (newStr[i] : 1);
+      // gonna look kinda like lines 10,11
     }
-}
-
-countLetters("lighthouse in the house")
+  }
+  console.log(output);
+};
+var output = {
+};
+countLetters("lighthouse in the house");
